59af303d9551f230428b91de675d75c967827aca
Ebenezer Munika <ebenezerperplex010@gmail.com>
2024-10-20 02:33:13 +0300

    Update Footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>Candy & Choco</p>
+        <p>nJFashion</p>
       </div>
       <ul className="footer-links">
         <li>Candy & Choco</li>
