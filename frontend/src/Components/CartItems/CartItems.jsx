eda921eb2737fb33dc900ec1a3a3ee429bab3a6b
Ebenezer Munika <ebenezerperplex010@gmail.com>
2025-03-21 11:34:12 +0300

    Update CartItems.jsx

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -55,7 +55,7 @@ const CartItems = () => {
           <div>
             <div className="cartitems-total-item">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>Ksh {getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cartitems-total-item">
@@ -65,7 +65,7 @@ const CartItems = () => {
             <hr />
             <div className="cartitems-total-item">
               <h3>Total</h3>
-              <h3>${getTotalCartAmount()}</h3>
+              <h3>Ksh {getTotalCartAmount()}</h3>
             </div>
             <button>PROCEED TO CHECKOUT</button>
           </div>
